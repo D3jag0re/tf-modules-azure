12717d8b82661c7aadf3657c97fe2ff18bb1639b
D3jag0re <91279878+D3jag0re@users.noreply.github.com>
2024-07-02 15:19:31 -0400

    Update outputs.tf

--- appServiceLinux/outputs.tf
+++ appServiceLinux/outputs.tf
@@ -3,19 +3,19 @@ output "app_service_name" {
 }
 
 output "app_service_id" {
-  value = azurerm_app_service.app_service_web.id
+  value = azurerm_linux_web_app.app_service_linux.id
 }
 
 output "app_service_managed_identity_principal_id" {
-  value       = try(azurerm_app_service.app_service_web.identity[0].principal_id, null)
+  value       = try(azurerm_linux_web_app.app_service_linux.identity[0].principal_id, null)
   description = "The principal ID of the system-assigned managed identity for the App Service. Null if no system-assigned identity is used."
 }
 
 output "app_service_details" {
   value = {
-    service_principal_name = azurerm_app_service.app_service_web.name
-    service_principal_id   = try(azurerm_app_service.app_service_web.identity[0].principal_id, null)
-    tags                   = azurerm_app_service.app_service_web.tags
+    service_principal_name = azurerm_linux_web_app.app_service_linux.name
+    service_principal_id   = try(azurerm_linux_web_app.app_service_linux.identity[0].principal_id, null)
+    tags                   = azurerm_linux_web_app.app_service_linux.tags
   }
 }
 
